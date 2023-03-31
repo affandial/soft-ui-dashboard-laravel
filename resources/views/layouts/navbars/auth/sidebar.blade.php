1f7d297f51742cf670cc109c07d096c1296323ab
Affandi Agung <affandi@datasintesa.id>
2023-03-31 09:40:15 +0700

    test

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -122,7 +122,8 @@
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i style="font-size: 1rem;"
                                 class="fas fa-lg fa-list-ul ps-2 pe-2 text-center text-dark {{ Request::is('today') ? 'text-white' : 'text-dark' }} "
-                                aria-hidden="true"></i>
+                                aria-hidden="true">
+                              </i>
                         </div>
                         <span class="nav-link-text ms-1">Jadwal</span>
                     </a>
